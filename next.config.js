4d7f6a513c45a89f995b3bbcaf3a4a9b76f9faec
Dwlad90 <dwlad90@gmail.com>
2024-05-18 15:21:21 +0300

    feat(nextConfig): transpile with swc stylex plugin

--- next.config.js
+++ next.config.js
@@ -1,12 +1,31 @@
 /** @type {import('next').NextConfig} */
-const stylexPlugin = require("@stylexjs/nextjs-plugin");
+const stylexPlugin = require("@stylexswc/nextjs-plugin");
 const withMDX = require("@next/mdx")();
+
 
 const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   transpilePackages: ["@stylexjs/open-props"],
   // Optionally, add any other Next.js config below
+  swcMinify: true,
+  transpilePackages: ["@stylexjs/open-props"],
+  experimental: {
+    mdxRs: true,
+    swcPlugins: [[
+      "@stylexswc/swc-plugin",
+      {
+        "dev": false,
+        "runtimeInjection": false,
+        "genConditionalClasses": true,
+        "treeshakeCompensation": true,
+        "unstable_moduleResolution": {
+          "type": "commonJS",
+          "rootDir": __dirname
+        }
+      }
+    ]],
+  },
 };
 
 module.exports = stylexPlugin({
